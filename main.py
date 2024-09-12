8a8de4858f932a2e3db6a94b8db4aa068dcec450
Kai-Wen Tuan <kevin810323@gmail.com>
2024-09-12 02:07:47 +0000

    update

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-#from feature import StatisticalFeatureTransformer, ErrorFeatureTransformer
+from feature import StatisticalFeatureTransformer, ErrorFeatureTransformer
 import pickle
 from predict import predict
 from utils import grader, gec_result, update_answers
-from flask import Flask, jsonify, request
+#from flask import Flask, jsonify, request
 import pandas as pd
 import spacy
 import re
@@ -14,6 +14,7 @@ bertscore = load("bertscore")
 
 
 from fastapi import FastAPI
+from fastapi.encoders import jsonable_encoder
 from pydantic import BaseModel
 from typing import List, Optional, Dict
 
@@ -28,7 +29,7 @@ mylogger.addHandler(handler)
 
 
 
-VERSION = "Aug19"
+VERSION = "Sep11"
 en_nlp = spacy.load('en_core_web_sm')
 
 with open('model.pkl', 'rb') as f:
@@ -44,7 +45,7 @@ class Question(BaseModel):
 
 class Test(BaseModel):
     test_id: int
-    short-answer-questions: Dict[str, Question]
+    short_answer_questions: Dict[str, Question]
     short_essay: Dict[str, str]
 
 app = FastAPI()
@@ -56,50 +57,56 @@ def default_response():
 
 
 @app.post("/grade/")
-async def test_process():
-    if request.is_json:
-        data = request.json
-        mylogger.info("Input data")
-        mylogger.info(data)
+async def test_process(data: Test):
+     
+    
+    mylogger.info("Input data")
+    mylogger.info(data)
+    
+
+    for qnum in data.short_answer_questions:
+        orig = data.short_answer_questions[qnum].orig
+        orig = re.sub("' s", "'s", orig)
+        c_ans = data.short_answer_questions[qnum].c_ans
+        temp = []
+        for ans in c_ans:
+            temp.append(re.sub("' s", "'s", ans))
+            
         
+        allowed_ans = update_answers(orig, temp)
 
-        for qnum in data['short-answer-questions']:
-            orig = data['short-answer-questions'][qnum]['orig']
-            orig = re.sub("' s", "'s", orig)
-            c_ans = data['short-answer-questions'][qnum]['c_ans']
-            temp = []
-            for ans in c_ans:
-                temp.append(re.sub("' s", "'s", ans))
-                
-            
-            allowed_ans = update_answers(orig, temp)
+        s_ans = data.short_answer_questions[qnum].s_ans
+        s_ans = re.sub("' s", "'s", s_ans)
+        grade = grader(allowed_ans, s_ans)
+        data.short_answer_questions[qnum].grade = grade['grade']
 
-            s_ans = data['short-answer-questions'][qnum]['s_ans']
-            s_ans = re.sub("' s", "'s", s_ans)
-            grade = grader(allowed_ans, s_ans)
-            data['short-answer-questions'][qnum]['grade'] = grade['grade']
-            
-        essay = data['short_essay']['text']
-        sample = data['short_essay']['sample']
+    mylogger.info(data.short_answer_questions)
+        
+    essay = data.short_essay['text']
+    sample = data.short_essay['sample']
 
-        topic_score = bertscore.compute(predictions=[essay], references=[sample], lang ='en', model_type="distilbert-base-uncased")['f1'][0]
-        if topic_score < 0.7:
-            essay_grade = 0
-        elif len(en_nlp(essay)) < 50:
-            essay_grade = 2
-        else:
-            df = pd.DataFrame({'text': [essay]}) 
-            essay_grade = model.predict(df)[0]
-        essay_result = gec_result(essay)
-        essay_result['essay_score'] = essay_grade
-
-        data["short_essay"] = essay_result
-    
+    topic_score = bertscore.compute(predictions=[essay], references=[sample], lang ='en', model_type="distilbert-base-uncased")['f1'][0]
+    if topic_score < 0.7:
+        essay_grade = 0
+    elif len(en_nlp(essay)) < 50:
+        essay_grade = 2
     else:
-        mylogger.info("No json call")
-        return "Not Json data"
+        df = pd.DataFrame({'text': [essay]}) 
+        essay_grade = model.predict(df)[0]
+    essay_result = gec_result(essay)
+    essay_result['essay_score'] = essay_grade
+
+    data.short_essay = essay_result
     
+
+    mylogger.info(data.short_essay)
 
     mylogger.info("Output data")
     mylogger.info(data)
-    return jsonify(data), 200
+    json_data = jsonable_encoder(data)
+    return json_data
+
+if __name__ == "__main__":
+    import uvicorn
+
+    uvicorn.run(app, host="127.0.0.1", port=8000, workers=4)
